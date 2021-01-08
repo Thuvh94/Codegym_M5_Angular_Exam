40b52fce9a293fc6c9a3349c2129deac03937450
unknown <aloha.hat.2508@gmail.com>
2021-01-08 10:34:54 +0700

    delete book

--- src/app/books/delete/delete.component.ts
+++ src/app/books/delete/delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {IBook} from '../../ibook';
+import {BooksService} from '../../service/books.service';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 
 @Component({
   selector: 'app-delete',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete.component.css']
 })
 export class DeleteComponent implements OnInit {
-
-  constructor() { }
+  book: IBook;
+  id: any;
+  constructor(private bookService: BooksService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.id = paramMap.get('id');
+      this.bookService.getBookById(this.id).subscribe((result) => {
+        this.book = result;
+      });
+    });
+  }
+
+  deleteBook(){
+    this.bookService.delete(this.id).subscribe((result) => {
+      alert('Xóa thánh công!');
+      this.router.navigate(['listBook']);
+    });
   }
 
 }
